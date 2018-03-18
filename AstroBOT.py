85b89bf83f87750f3716ced099a12db17c1afc95
Carlos Daniel Trejos Angulo <zekrom@MacBook-Pro-de-Carlos.local>
2018-03-18 17:17:04 -0600

    Código más limpio y optimizado

--- AstroBOT.py
+++ AstroBOT.py
@@ -42,12 +42,11 @@ def inicio(bot, update):
 
     if id_usuario in WhiteList:
 
-        teclado = [["Un saludo al bot"], ["Contacto"+emoji.emojize(':v:',  use_aliases=True)], ["¿Y cómo estuvo su dia?", "¿Cómo hizo esto?"],  [
-            "No hace nada", "Algo interesante"], ["Optical Character Recognition", "Chao"]]
+        teclado = [["Un saludo al bot"], ["Contacto"+emoji.emojize(':v:',  use_aliases=True)], ["Chao"]]
         reply_markup = ReplyKeyboardMarkup(teclado, resize_keyboard=True)
 
         bot.sendMessage(chat_id=id_usuario, text="¡Hola!"+" "+user.first_name +
-                    " (Si ocupás ayuda presiona \help)\n\nMarque una opción del teclado: ", reply_markup=reply_markup)
+                    " (Si ocupás ayuda presiona /help)\n\nMarque una opción del teclado: ", reply_markup=reply_markup)
     
     else:
         teclado = []
@@ -75,58 +74,10 @@ def aiuda(bot, update):
 
     bot.sendMessage(chat_id= id_usuario, text= "¡AIUDAAAAAAAAA!")
 
-    bot.sendMessage(chat_id= id_usuario, text= "Siempre podés reiniciar el chat con el comando \start \n\nSi ocupás ayuda con otra vara escribime a @jfeng23")
+    bot.sendMessage(chat_id= id_usuario, text= "Siempre podés reiniciar el chat con el comando /start \n\nSi ocupás ayuda con otra vara escribile a @jfeng23 o a @CdTrejos")
 
     print("\n") 
 
-    
-
-def imagenes(bot, update):
-
-    print(update)
-
-    id_usuario = update.message.chat_id
-
-    bot.sendMessage(chat_id= id_usuario, text= "Foto recibida")
-
-    archivo = bot.getFile(update.message.photo[-1].file_id)
-
-    archivo.download("imagen.jpg")
-
-    texto1 = ocr_space("imagen.jpg")
-
-    bot.sendMessage(chat_id= id_usuario, text= texto1)
-
-    print("\n")
-
-def ocr_space(filename, overlay=False, api_key='11a3e6dfb388957', language='spa'):
-
-    # print(update)
-
-    # Función para extraer el texto de una imagen
-    # Cambiar "helloworld" por el id dado por OCR.space después de registrarse 
-
-    payload = {'isOverlayRequired': overlay,
-               'apikey': api_key,
-               'language': language,
-               }
-    with open(filename, 'rb') as f:
-        r = requests.post('https://api.ocr.space/parse/image',
-                          files={filename: f},
-                          data=payload,
-                          )
-
-    respuesta = json.loads(r.content.decode())
-    print(respuesta)
-
-    print("\n")
-
-    try:
-        texto = respuesta["ParsedResults"][0]["ParsedText"]
-    except:
-        texto = "No encontré texto"
-
-    return texto
 
 def saludo(bot, update):
 
@@ -138,73 +89,7 @@ def saludo(bot, update):
     bot.sendMessage(chat_id= id_usuario, text= "¡Saludos"+" "+user.first_name+"!"+emoji.emojize(':v:',  use_aliases=True))
 
     print("\n")
-
-
-def dia(bot, update):
-
-    print(update)
-
-    id_usuario = update.message.chat_id
-
-    bot.sendMessage(chat_id= id_usuario, text= emoji.emojize('Estuvo demasiado atareado :persevere:', use_aliases=True))
-
-    print("\n")
-
-
-def comoHizoEsto(bot, update):
-
-    print(update)
-
-    id_usuario = update.message.chat_id
-
-    bot.sendMessage(chat_id= id_usuario, text= "De hecho que es vacilón, porque después de varios días de buscar en internet exhaustivamente, debuggear y ser un intenso con el mae que hizo el bot del TEC... Finalmente pude encontrar los errores :'D Pero más que todo fue por un taller que hiceron de bots y me emocioné haciendo algo bien cosi xD")
-
-    print("\n")
     
-
-def algo(bot, update):
-
-    print(update)
-
-    id_usuario = update.message.chat_id
-
-    bot.sendMessage(chat_id= id_usuario, text= "\"No hace nada\" tiene un secreto...")
-
-    global varCosi
-    varCosi = 1
-
-    print("\n")
-     
-    
-varCosi = 0
-
-
-def opcion(bot, update):
-
-    print(update)
-
-    id_usuario = update.message.chat_id
-
-    if(varCosi == 0):
-
-        bot.sendMessage(chat_id= id_usuario, text= "No hace nada :v")
-
-    if(varCosi == 1):
-
-        bot.sendMessage(chat_id= id_usuario, text= "La quiero mucho ❤️ y perdón por hacerla esperar ")
-
-    print("\n")
-        
-
-def OCR(bot, update):
-
-    print(update)
-
-    id_usuario = update.message.chat_id 
-
-    bot.sendMessage(chat_id= id_usuario, text= "Intente mandar una imagen que tenga texto para intentar sacarle el texto que tiene :)")
-    
-    print("\n")
 
 def contact(bot, update):
 
@@ -313,16 +198,9 @@ updater.dispatcher.add_handler(CommandHandler("start", inicio))
 
 updater.dispatcher.add_handler(CommandHandler("help", aiuda))
 
-updater.dispatcher.add_handler(MessageHandler(Filters.photo, imagenes))
-
 updater.dispatcher.add_handler(RegexHandler("Un saludo al bot", saludo))
 updater.dispatcher.add_handler(RegexHandler("Contacto",contact))
-updater.dispatcher.add_handler(RegexHandler("¿Y cómo estuvo su dia?", dia))
-updater.dispatcher.add_handler(RegexHandler("¿Cómo hizo esto?", comoHizoEsto))
-updater.dispatcher.add_handler(RegexHandler("Algo interesante", algo))
-updater.dispatcher.add_handler(RegexHandler("No hace nada", opcion))
-updater.dispatcher.add_handler(RegexHandler("Optical Character Recognition", OCR))
-updater.dispatcher.add_handler(RegexHandler("Contacto",contact))
+
 updater.dispatcher.add_handler(RegexHandler("soyastroteco", password))
 updater.dispatcher.add_handler(RegexHandler("Chao", bye))
 updater.dispatcher.add_handler(RegexHandler("Hello", hello))
